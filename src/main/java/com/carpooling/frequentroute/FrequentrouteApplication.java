3e4411184dd171cb2cf6739182f3c5157c0f100d
vuducminh <vuducminhkyle@gmail.com>
2021-04-16 23:27:48 +0700

    first commit

---
+++ src/main/java/com/carpooling/frequentroute/FrequentrouteApplication.java
@@ -0,0 +1,13 @@
+package com.carpooling.frequentroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrequentrouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrequentrouteApplication.class, args);
+    }
+
+}
